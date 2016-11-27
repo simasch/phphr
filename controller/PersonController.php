a67dccae46bac4bdf94914f5e22449d86bd41de7
Simon Martinelli <sm@simas.ch>
2016-11-27 11:16:35 +0100

    added log4php

--- controller/PersonController.php
+++ controller/PersonController.php
@@ -3,39 +3,52 @@
 include_once 'model/Person.php';
 include_once 'dao/PersonDao.php';
 
+include_once 'log4php/Logger.php';
+
 class PersonController
 {
     private $personDao;
+    private $logger;
 
     public function __construct()
     {
         $this->personDao = new PersonDao();
+
+        Logger::configure('log4php.xml');
+
+        $this->logger = Logger::getLogger('PersonController');
     }
 
     public function invoke()
     {
+        $this->logger->info("invoke()");
+
         $message = '';
 
-        if (isset($_POST['id'])) {
+        switch ($_SERVER['REQUEST_METHOD']) {
+            case 'GET':
+                if (isset($_GET['id'])) {
+                    $person = $this->personDao->find($_GET['id']);
+                    include 'view/person/edit.php';
+                } else {
+                    $people = $this->personDao->listPeople();
+                    include 'view/person/list.php';
+                }
+                return;
+            case 'POST':
+                if (isset($_POST['id'])) {
+                    $person = $this->personDao->find($_POST['id']);
+                    $person->setName($_POST['name']);
+                    $person = $this->personDao->update($person);
 
-            $person = $this->personDao->find($_POST['id']);
-            $person->setName($_POST['name']);
-            $person = $this->personDao->update($person);
+                    $message = 'Person saved';
 
-            $message = 'Person saved';
-
-            include 'view/person/edit.php';
-
-        } else if (isset($_GET['id'])) {
-
-            $person = $this->personDao->find($_GET['id']);
-            include 'view/person/edit.php';
-
-        } else {
-
-            $people = $this->personDao->listPeople();
-            include 'view/person/list.php';
-
+                    include 'view/person/edit.php';
+                }
+                return;
+            default:
+                http_response_code(405);
+                return;
         }
     }
 
